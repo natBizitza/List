21481d620375292ba1436a26c74777bfacae50eb
Natalia Romankevich <40891986+natBizitza@users.noreply.github.com>
2018-10-18 22:10:42 +0200

    upd

--- Elene de Troya/Elene de Troya/Program.cs
+++ Elene de Troya/Elene de Troya/Program.cs
@@ -10,6 +10,17 @@ namespace Elene_de_Troya
     {
         static void Main(string[] args)
         {
+            //Create a horse with a capacity of 20 warriors.
+            CaballoDeTroya c1 = new CaballoDeTroya(20);
+            //Create 3 Greek warriors (Achilles, Agamenon, and Ajax). 
+            Griego g1 = new Griego("Achilles", 30, 6);
+            Griego g2 = new Griego("Agamenon", 32, 6);
+            Griego g3 = new Griego("Ajax", 31, 6);
+
+            //and mount them on the horse
+            Griego.AddGriegoInHorse(g1);
+
+
         }
     }
 }
