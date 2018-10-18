7405377c999b0e8831775d8c07db1634f0c6222e
Natalia Romankevich <40891986+natBizitza@users.noreply.github.com>
2018-10-18 11:59:14 +0200

    upd

---
+++ Elene de Troya/Elene de Troya/CaballoDeTroya.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elene_de_Troya
+{
+    class CaballoDeTroya
+    {
+        //        capacidad, representa el número de guerreros griegos que puede haber dentro del caballo.
+        //            No se podrá cambiar una vez le hemos dado valor.
+        //ocupacion, representa el número actual de griegos en el caballo.
+        //ocupantes, lista de objetos de la clase Guerrero.
+        private int capacity;
+        private int ocupation;
+        private string ocupants;
+
+        //Crear un constructor para la clase 
+        //Caballo con espacio para tantos guerreros como indique el parámetro que se pasa a dicho constructor. 
+
+        public CaballoDeTroya(int ocupation)
+        {
+            this.capacity = capacity;
+            this.ocupation = ocupation;
+        }
+    }
+}
